f087af93ad2d532dc1fd2679c1e54137d4cdb479
7onetella <7onetella@gmail.com>
2018-05-06 01:35:32 -0400

    adding seige

---
+++ cmd/seige.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"github.com/7onetella/mvk/internal/dockerw"
+	"github.com/spf13/cobra"
+)
+
+var siegeCmd = &cobra.Command{
+	Use:                "siege <siege parameters>",
+	Short:              "Runs siege",
+	Long:               `Runs siege`,
+	Example:            "siege -d1 -r10 -c25 example.com",
+	DisableFlagParsing: true,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		if len(args) < 1 {
+			cmd.Usage()
+			return
+		}
+
+		params := args[0:]
+
+		env := []string{}
+		dockerCmd := []string{"siege"}
+		for _, p := range params {
+			dockerCmd = append(dockerCmd, p)
+		}
+
+		dockerw.StartDockerConsole("yokogawa/siege", env, dockerCmd)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(siegeCmd)
+}
